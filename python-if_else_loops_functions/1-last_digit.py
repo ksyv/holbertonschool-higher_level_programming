2ae548d15d45d48f14ac43bf393c01055c5e4f63
ksyv <viais.kevin@hotmail.fr>
2024-01-23 10:27:11 +0100

    test fix the last charact of the first string

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ if number >= 0:
     lastdigit = number % 10
 else:
     lastdigit = -(number % -10)
-print(f"Last digit of {number} is {lastdigit}", end=" ")
+print(f"Last digit of {number} is {lastdigit}", end="")
 if lastdigit > 5:
-    print("and is greater than 5")
+    print(" and is greater than 5")
 elif lastdigit == 0:
-    print("and is 0")
+    print(" and is 0")
 else:
-    print("and is less than 6 and not 0")
+    print(" and is less than 6 and not 0")
