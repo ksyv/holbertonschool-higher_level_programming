48f0b5bd5f8a5b301e3d5dcb5bda58b73a0d2c01
ksyv <viais.kevin@hotmail.fr>
2024-01-24 10:27:35 +0100

    change import format

--- python-import_modules/1-calculation.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import calculator_1 as math
-if __name__ == '__main__':
-    a = 10
-    b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, math.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, math.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, math.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, math.div(a, b)))
