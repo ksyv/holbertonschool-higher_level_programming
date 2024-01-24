dbe64a18b6dfffb7a55e8566a77120a30352a31b
ksyv <viais.kevin@hotmail.fr>
2024-01-24 15:57:38 +0100

    fix typo in variable name

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     import hidden_4
     nameslist = dir(hidden_4)
-    for name in namesList:
+    for name in nameslist:
         if "__" != name[:2]:
             print(name)
