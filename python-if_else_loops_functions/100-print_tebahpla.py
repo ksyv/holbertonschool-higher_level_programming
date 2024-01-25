24182ccfd83e81cef23897c4201279ad112ad027
ksyv <viais.kevin@hotmail.fr>
2024-01-25 09:48:01 +0100

    add 100-.py

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for lettre in range(ord('z'), ord('a') - 1, -1):
+    if lettre % 2 != 0:
+        upper = 32
+    else:
+        upper = 0        
+    print("{:c}".format(lettre - upper), end="")
+
