70a7565b032aa8755b1f6dde4b4ce105c15ba798
ksyv <viais.kevin@hotmail.fr>
2024-01-25 13:02:39 +0100

    pycode fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    operator = {'+', '-', '\*', '/'}
+    operator = {'+', '-', '*', '/'}
     if argv[2] not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
