d2ced72f3c267ea7a67b64bd340f73f38c667282
ksyv <viais.kevin@hotmail.fr>
2024-01-25 12:38:59 +0100

    add 100.py

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit (1)
+    match (argv[2]):
+        case '+' | '-' | '*' | "/":
+            a = int(argv[1])
+            b = int(argv[3])
+            if argv[2] == '+':
+                result = add(a, b)
+            elif argv [2] == '-':
+                result = sub(a,b)
+            elif argv [2] == '*':
+                result = mul(a,b)
+            elif argv [2] == '/':
+                result = div(a,b)
+            print("{} {} {} = {}".format(a, argv[2], b, result))
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit (1)
