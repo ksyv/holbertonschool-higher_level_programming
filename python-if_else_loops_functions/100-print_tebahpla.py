62e63f712a09dee0db218af45162ea2762a633b3
ksyv <viais.kevin@hotmail.fr>
2024-01-25 09:49:31 +0100

    fix pycode style

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,6 +3,5 @@ for lettre in range(ord('z'), ord('a') - 1, -1):
     if lettre % 2 != 0:
         upper = 32
     else:
-        upper = 0        
+        upper = 0
     print("{:c}".format(lettre - upper), end="")
-
