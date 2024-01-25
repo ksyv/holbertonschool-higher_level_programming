383653542c1c86c3c4dae76762b0715aa24d6912
ksyv <viais.kevin@hotmail.fr>
2024-01-25 13:01:40 +0100

    use list for operator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,19 +5,18 @@ if __name__ == '__main__':
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    match (argv[2]):
-        case '+' | '-' | '*' | "/":
-            a = int(argv[1])
-            b = int(argv[3])
-            if argv[2] == '+':
-                result = add(a, b)
-            elif argv[2] == '-':
-                result = sub(a, b)
-            elif argv[2] == '*':
-                result = mul(a, b)
-            elif argv[2] == '/':
-                result = div(a, b)
-            print("{} {} {} = {}".format(a, argv[2], b, result))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    operator = {'+', '-', '\*', '/'}
+    if argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == '+':
+        result = add(a, b)
+    elif argv[2] == '-':
+        result = sub(a, b)
+    elif argv[2] == '*':
+        result = mul(a, b)
+    elif argv[2] == '/':
+        result = div(a, b)
+    print("{} {} {} = {}".format(a, argv[2], b, result))
