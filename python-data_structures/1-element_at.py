9c9a92912bade91911336f9a37a41a65a1f20b72
ksyv <viais.kevin@hotmail.fr>
2024-01-29 10:11:34 +0100

    add 1.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """retrives an element from a list
+        my-list is the list
+        idx is the searched element"""
+    if idx < 0 or idx > len(my_list): #if idx is negative or out of range, return None
+        return
+    return my_list[idx] #return value of element in idx
+    
