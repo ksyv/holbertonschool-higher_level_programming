77813bf37e35825640b58609221e9874bc651afe
ksyv <viais.kevin@hotmail.fr>
2024-01-29 12:57:29 +0100

    add 9.py

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max = 0
+    for integer in my_list:
+        if max < integer:
+            max = integer
+    return max
