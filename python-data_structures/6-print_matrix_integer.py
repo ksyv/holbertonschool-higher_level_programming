b1deb1591688773018a89f554c49a051f89ff2de
ksyv <viais.kevin@hotmail.fr>
2024-01-29 11:43:22 +0100

    change condition end of range

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for first_level in matrix:
-        for integer in range(0, len(first_level)):
+        for integer in range(0, len(first_level) - 1):
             if integer < len(first_level) - 1:
                 print("{:d}".format(first_level[integer]), end =" ")
             else:
