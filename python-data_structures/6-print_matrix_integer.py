284988bd8db6a701a312e34029075b80c42fceaa
ksyv <viais.kevin@hotmail.fr>
2024-01-29 11:47:27 +0100

    fix pycode style

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for first_level in matrix:
         for integer in range(0, len(first_level)):
             if integer < len(first_level) - 1:
-                print("{:d}".format(first_level[integer]), end =" ")
+                print("{:d}".format(first_level[integer]), end=" ")
             else:
                 print("{:d}".format(first_level[integer]))
