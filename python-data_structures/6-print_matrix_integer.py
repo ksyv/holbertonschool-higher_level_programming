df5487298db08c873e580b07d3445f5326f19572
ksyv <viais.kevin@hotmail.fr>
2024-01-29 11:22:39 +0100

    delete last change

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for first_level in matrix:
         for integer in first_level:
             print("{:d}".format(integer), end =" ")
-        print("$")
+        print("")
