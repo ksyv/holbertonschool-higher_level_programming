9119a349884fa553d46151a45e45516a24d4e0ef
ksyv <viais.kevin@hotmail.fr>
2024-01-29 11:35:04 +0100

    change condition for space at the end of the line

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for first_level in matrix:
-        for integer in first_level:
-            print("{:d}".format(integer), end =" ")
-        print("")
+        for integer in range(0, len(first_level)):
+            if integer < len(first_level) - 1:
+                print("{:d}".format(first_level[integer]), end =" ")
+            else:
+                print("{:d}".format(first_level[integer]), end ="")
+        print("$")
