8ccc3518f0da331ecad6fa1bfad7a6afa1b80dcc
ksyv <viais.kevin@hotmail.fr>
2024-01-30 09:45:40 +0100

    delete firsts condition

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return
-    if not search or not replace:
-        return my_list
     new_list = []
     for index in my_list:
         if index is not search:
