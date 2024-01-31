a71647d7910339af241a8a01579a248588082cb1
ksyv <viais.kevin@hotmail.fr>
2024-01-31 13:32:02 +0100

    add 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    real_number = 0
+    for number in range(x):
+        try:
+            print("{:d}".format(my_list[number]), end="")
+            real_number += 1
+        except (TypeError, ValueError):
+            continue
+        except IndexError:
+            break
+    print("")
+    return real_number
