66df37139e7d452b302609a403222a15ba52e1b6
ksyv <viais.kevin@hotmail.fr>
2024-02-05 14:44:34 +0100

    add 5-.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""square modul"""
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size=0):
+        """init a new square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """retrieve size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ def if size is an >0 int in private attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ define the area of a square entrer in argument"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """print in stdout a # square of size __size"""
+        for index in range(0, self.__size):
+            for index2 in range(0, self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
