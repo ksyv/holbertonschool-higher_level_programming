458edbda77a1b61034cf2b8484d7b4d1fceb4ee5
ksyv <viais.kevin@hotmail.fr>
2024-02-05 12:45:15 +0100

    add 1;py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""square modul"""
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size):
+        """ init a new square with his size in private attribute"""
+        self.__size = size
