54813570e1f0bcc67a45d0ea719266d7aacbb2af
ksyv <viais.kevin@hotmail.fr>
2024-02-05 13:29:21 +0100

    add 3-.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""square modul"""
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size=0):
+        """ init new square with his size is an >0 int in private attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ define the area of a square entrer in argument"""
+        self.area = self.__size ** 2
+        return self.area
