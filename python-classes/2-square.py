6d01af8b1430cf4fd5ba6622e416c5e2e2d54b71
ksyv <viais.kevin@hotmail.fr>
2024-02-05 13:12:03 +0100

    add 2-.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""square modul"""
+
+
+class Square:
+    """class that defines a square"""
+    def __init__(self, size=0):
+        """ init new square with his size is an >0 int in private attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must b >= 0")
+        self.__size = size
