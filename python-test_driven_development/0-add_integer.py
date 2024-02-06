e993df5690fd8a94c07f587b716e9d66d697781f
ksyv <viais.kevin@hotmail.fr>
2024-02-06 13:07:06 +0100

    add new folder, readme, test folder, and 0-.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""modul for the first task, Write a function that adds 2 integers."""
+
+
+def add_integer(a, b=98):
+    """return int sum of a and b if they are int or float"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a + b)
