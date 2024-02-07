332f96b64bd55ffe948072a6c3eb99700fbedbb2
ksyv <viais.kevin@hotmail.fr>
2024-02-07 11:26:44 +0100

    add 5.py and .txt

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""modul for the task: Write a function that prints\
+a text with 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """run text and replace specfied char and space by 2 new line"""
+    if not isinstance(text , str):
+        raise TypeError("text must be a string")
+    specify_char = ['.', '?', ':', ',']
+    first_char = 0
+    index = 0
+    for test_char in text:
+        if test_char in specify_char:
+            char_index = index + 1
+            print("{}\n".format(text[first_char:char_index]))
+            first_char = char_index + 1
+        index += 1
