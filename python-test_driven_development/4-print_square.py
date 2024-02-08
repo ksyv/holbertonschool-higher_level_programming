c27a76c0b4d838f4bedf4279887338993237d712
ksyv <viais.kevin@hotmail.fr>
2024-02-08 09:26:47 +0100

    fix pycodestyle

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -11,6 +11,7 @@ def print_square(size):
     for edge in range(size):
         print("#" * size)
 
+
 if __name__ == '__main__':
     import doctest
     doctest.testfile("tests/4-print_square.txt")
