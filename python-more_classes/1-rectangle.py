c1a256d321c714e65098ee91bbcb6e1291a7a569
ksyv <viais.kevin@hotmail.fr>
2024-02-08 13:20:45 +0100

    add 1.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""square modul"""
+
+
+class Rectangle:
+    """class that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """init new rectangle with his width and heigth"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0") 
+        self.__width = width
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0") 
+        self.__height = height
