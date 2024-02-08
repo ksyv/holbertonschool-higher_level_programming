24e6a51aba57fdeab3a79a43961f1f1e6faf1e3e
ksyv <viais.kevin@hotmail.fr>
2024-02-08 12:48:37 +0100

    try t change comment

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""modul for the first task, Write a function that adds 2 integers."""
+"""modul for the first task, Write a function that adds 2 integers.
+"""
 
 
 def add_integer(a, b=98):
-    """return int sum of a and b if they are int or float"""
+    """return int sum of a and b if they are int or float
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
