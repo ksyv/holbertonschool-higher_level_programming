f5c1b7534469bbbab0d19f6bd93433d983bae9cd
ksyv <viais.kevin@hotmail.fr>
2024-02-08 11:52:15 +0100

    fix pycode

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,20 +12,24 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                        (list of lists) of integers/floats")
     try:
         size = len(matrix[0])
     except Exception:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix \
+                        (list of lists) of integers/floats")
     for i in matrix:
         if not isinstance(i, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix \
+                            (list of lists) of integers/floats")
         if len(i) != size:
             raise TypeError("Each row of the matrix must have the same size")
         new_row = []
         for number in i:
             if not isinstance(number, int) and not isinstance(number, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix \
+                                (list of lists) of integers/floats")
             initial = float(number / div)
             result = float("{:.2f}".format(initial))
             new_row.append(result)
