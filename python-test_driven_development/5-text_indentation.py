c9525ca97593408704f78d53e3c4de86fcfc6dbe
ksyv <viais.kevin@hotmail.fr>
2024-02-08 09:25:59 +0100

    fix pycodestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,7 +5,7 @@ a text with 2 new lines after each of these characters: ., ? and :"""
 
 def text_indentation(text):
     """run text and replace specfied char and space by 2 new line"""
-    if not isinstance(text , str):
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
     specify_char = ['.', '?', ':', ',']
     first_char = 0
@@ -16,6 +16,7 @@ def text_indentation(text):
             print("{}\n".format(text[first_char:char_index]))
             first_char = char_index + 1
         index += 1
+
 
 if __name__ == '__main__':
     import doctest
