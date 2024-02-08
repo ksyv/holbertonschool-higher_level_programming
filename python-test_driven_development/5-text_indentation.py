73c0e859eb21f0d28c6ae755f562079d7bc1ee18
ksyv <viais.kevin@hotmail.fr>
2024-02-08 11:48:09 +0100

    change comment

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,13 +4,7 @@ a text with 2 new lines after each of these characters: ., ? and :"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
-    """
+    """Print text with two new lines after each '.', '?', and ':'"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
