532715776eab0eb26db74915271a07437de4afad
ksyv <viais.kevin@hotmail.fr>
2024-02-08 13:46:05 +0100

    fix changement of variable name

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,9 +17,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """def if width is an int >= 0"""
-        if not isinstance(width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -31,8 +31,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """def if height is an int >= 0"""
-        if not isinstance(height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
