aba02493d35ee5a814b8fae5cf2e3341d5033998
ksyv <viais.kevin@hotmail.fr>
2024-02-13 11:35:09 +0100

    add 2-.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns True if the object is exactly an instance
+of the specified class ; otherwise False"""
+
+
+def is_same_class(obj, a_class):
+    """check if object is a a_class intance"""
+    if isinstance(obj, a_class):
+        return True
+    return False
