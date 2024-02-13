4c97887b508d99a3faff5740f77718ecd1af65a5
ksyv <viais.kevin@hotmail.fr>
2024-02-13 12:55:43 +0100

    add 6.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """_summary_
+    """
+    def area(self):
+        """_summary_
+
+        Raises:
+            Exception: _description_
+        """
+        raise Exception("area() is not implemented")
