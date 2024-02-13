6feda3ec5c304f43897e678a5bde173173f9fa66
ksyv <viais.kevin@hotmail.fr>
2024-02-13 15:50:13 +0100

    change doc

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
+"""write a class Rectangle that inherits from BaseGeometry."""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """rectangle using BaseGeometry."""
     def __init__(self, width, height):
         """Intialize a new Rectangle.
 
