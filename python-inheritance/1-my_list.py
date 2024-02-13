42b6a41b5a5cb317bd768f131101405a1c91ba5d
ksyv <viais.kevin@hotmail.fr>
2024-02-13 10:45:00 +0100

    add 1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""modul write a class that inherits from another"""
+
+class MyList(list):
+    """prints the list sorted in ascending sort"""
+    def print_sorted(self):
+        """prints the list sorted in ascending sort"""
+        print(sorted(self))
