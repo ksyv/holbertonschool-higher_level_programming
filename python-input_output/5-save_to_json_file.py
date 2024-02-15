9406d0c1610039b3153cb05f8da96a43f079c1c4
ksyv <viais.kevin@hotmail.fr>
2024-02-15 10:56:25 +0100

    add 5.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module for define a function that writes an
+Object to a text file, using a JSON representation """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation:"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        json.dump(my_obj, file)
