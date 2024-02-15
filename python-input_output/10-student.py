4e10996742aa8abd06ac7d7682943b7b2f3a9ad7
ksyv <viais.kevin@hotmail.fr>
2024-02-15 12:40:29 +0100

    10.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student"""
+
+
+class Student():
+    """Defines the Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Creates a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+
+        new_dic = {}
+        for i in attrs:
+            try:
+                new_dic[i] = self.__dict__[i]
+            except:
+                pass
+
+        return new_dic
