783f09c4ae41ed04ad3c534bc1a534ff9c72f17d
ksyv <viais.kevin@hotmail.fr>
2024-02-19 14:57:30 +0100

    Update the class Rectangle by overriding the __str__ method so that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,8 @@ class Rectangle(Base):
         """print rectangle instance with "#" """
         for index in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        '''Overwritting the str method'''
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                       self.width, self.height)
