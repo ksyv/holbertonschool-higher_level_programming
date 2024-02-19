c97d31da4afa04b4bda1f31c9372d9ec9bcf76f8
ksyv <viais.kevin@hotmail.fr>
2024-02-19 14:10:21 +0100

    Update the class Rectangle by adding the public method def area(self): that returns the area value of the Rectangle instance.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        """ define the area of the rectangle"""
+        return (self.__height * self.__width)
