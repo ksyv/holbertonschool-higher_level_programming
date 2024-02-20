b026e4bfdc3678085aca267917835392af2e40e3
ksyv <viais.kevin@hotmail.fr>
2024-02-20 14:34:50 +0100

    Update the class Base by adding the class method def create(cls, **dictionary): that returns an instance with all attributes already set

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """modul for create a Base class"""
+import json
 
 
 class Base:
@@ -17,3 +18,43 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or not json_string:
+            return "[]"
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file
+        list_objs is a list of instances who inherits of Base
+        filename is <class name>.json"""
+        filename = cls.__name__ + ".json"
+        listOf = []
+        if list_objs is not None:
+            for object in list_objs:
+                listOf.append(cls.to_dictionary(object))
+        with open(filename, 'w') as jsonFileSaved:
+            jsonFileSaved.write(cls.to_json_string(listOf))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ is "Rectangle":
+            dummy = cls(3, 4)
+        elif cls.__name__ is "Square":
+            dummy = cls(3)
+        dummy.update(**dictionary)
+        return dummy
+    
