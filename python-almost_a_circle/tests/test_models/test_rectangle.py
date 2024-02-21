888b765f9c206b1514537c4fab2a87f7268020d8
ksyv <viais.kevin@hotmail.fr>
2024-02-21 17:02:27 +0100

    fix pycodestyle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -79,15 +79,16 @@ class TestRectangleArea(unittest.TestCase):
         with self.assertRaises(TypeError):
             rectangle1.area(1)
 
+
 class Testupdate(unittest.TestCase):
     """test the update method"""
     def testUpdate(self):
         rectangle1 = Rectangle(10, 10, 10, 10)
         rectangle1.update(89, 2)
         rectangle1.update(x=1, height=2, y=3, width=4)
-        self.assertEqual([rectangle1.id, rectangle1.width, rectangle1.height, rectangle1.x, rectangle1.y],
-                         [89, 4, 2, 1, 3] )
-        
+        self.assertEqual([rectangle1.id, rectangle1.width, rectangle1.height,
+                          rectangle1.x, rectangle1.y], [89, 4, 2, 1, 3])
+
 
 if __name__ == "__main__":
     unittest.main()
