27121fd93bbbc51c5e486e8c4e6c1ae7ac274f42
ksyv <viais.kevin@hotmail.fr>
2024-03-22 13:08:02 +0100

    fix title

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-USE hbtn_0d_tvshows_rate;
+
 SELECT tv_shows.title, SUM(rate) as rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.id
+GROUP BY tv_shows.title
 ORDER BY rating DESC;
