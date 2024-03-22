e76fc225424d7b5126f53cdafa6a8133eb47c525
ksyv <viais.kevin@hotmail.fr>
2024-03-22 09:35:40 +0100

    try

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,9 +4,12 @@ import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
+def main():
+    """start of prog"""
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    if (sys.argv[4] == "'"):
+        return
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name= '{}' \
                 ORDER BY id".format(sys.argv[4]))
@@ -14,3 +17,7 @@ if __name__ == "__main__":
     for state in states:
         if state[1] == sys.argv[4]:
             print(state)
+
+
+if __name__ == "__main__":
+    main()
