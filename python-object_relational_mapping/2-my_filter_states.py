4acba439a8186e039e965ef79d8315043cb5d0c1
ksyv <viais.kevin@hotmail.fr>
2024-03-22 09:12:01 +0100

    more simple

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name= '{}' \
-                ORDER BY states.id".format(sys.argv[4]))
+                ORDER BY id".format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
