101044b245d806039e0f842e354bdbba419a711f
ksyv <viais.kevin@hotmail.fr>
2024-03-22 12:45:32 +0100

    add 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def main():
+    """Programm strt here"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], "", sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_with_a = session.query(State).filter(State.name.like("%a%")).all()
+    for state in states_with_a:
+        session.delete(state)
+    session.commit()
+
+
+
+if __name__ == '__main__':
+    main()
