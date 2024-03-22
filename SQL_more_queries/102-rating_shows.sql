da33f0776178eb9930fb1cfc92e27e44582dc7ca
ksyv <viais.kevin@hotmail.fr>
2024-03-22 13:05:26 +0100

    add use

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,5 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+USE hbtn_0d_tvshows_rate;
 SELECT tv_shows.title, SUM(rate) as rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
